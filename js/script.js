8edd3107183fd73037aec61e8ed993d9c13e805e
Riccardo Potalivo <riccardopotalivo@gmail.com>
2023-10-31 15:58:42 +0100

    milestone-1-set

--- js/script.js
+++ js/script.js
@@ -3,12 +3,28 @@
 const {createApp} = Vue
 
 createApp({
-    
+
     data(){
-
+        return {
+            // array tasks
+            tasks: [],
+            
+        }
+        
     },
-
+    
     methods: {
+        // nuova task
+        addTask(){
+            const newTask = {
+                text: 'aa',
+                id: 1,
+                done: false
+            }
+
+            this.tasks.push(newTask)
+            console.log(tasks)
+        }
 
     },
 
